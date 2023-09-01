699ff969db133a264858e5d2b3bd7e73c432817a
Luan Paulino Marcelino <132668774+luanpm7@users.noreply.github.com>
2023-09-01 11:13:34 +0000

    lalala

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";
-import { selectUsuarios } from "./bd.js";
+import { selectUsuarios, selectUsuario } from "./bd.js";
 
 dotenv.config();
 
@@ -30,4 +30,14 @@ app.get("/usuarios", async (req, res) => {
 app.listen(port, () => {
   // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
+});
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
